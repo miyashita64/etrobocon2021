69b0a09002d9e05d34966f06e6a2d92118440e3f
Takahiro55555 <tkhr.corei7@gmail.com>
2021-05-17 18:30:56 +0900

    delete: Controller クラスと Display クラスを削除

--- module/EtRobocon2021.cpp
+++ module/EtRobocon2021.cpp
@@ -1,6 +1,4 @@
 #include "EtRobocon2021.h"
-#include "Controller.h"
-#include "Display.h"
 
 /*
 * タッチセンサーを押すと直進する
@@ -9,41 +7,4 @@
 */
 void EtRobocon2021::start()
 {
-  // Controllerクラスを使うためにインスタンス化する
-  Controller controller;
-
-  while (true)
-  {
-    // タッチセンサーを押すと直進する
-    if (controller.touchSensor.isPressed())
-    {
-      controller.setLeftMotorPwm(30);
-      controller.setRightMotorPwm(30);
-    }
-
-    // エンターボタンを押すと停止する
-    if (controller.buttonIsPressedEnter())
-    {
-      controller.stopMotor();
-    }
-
-    // 下ボタンを押すとカラーセンサーのRGB値を表示する
-    if (controller.buttonIsPressedDown())
-    {
-      rgb_raw_t rgb;
-      controller.colorSensor.getRawColor(rgb);
-      printf("R:%3d, G:%3d, B:%3d\n", rgb.r, rgb.g, rgb.b);
-      // 走行体の画面に表示する(動かない)
-      Display::print(6, "R:%3d, G:%3d, B:%3d", rgb.r, rgb.g, rgb.b);
-    }
-
-    // バックボタンを押すと終了する
-    if (controller.buttonIsPressedBack())
-    {
-      break;
-    }
-
-    // タスク時間4０００マイクロ秒=4ミリ秒待つ while文では必要らしい
-    controller.tslpTsk(4000);
-  }
 }
